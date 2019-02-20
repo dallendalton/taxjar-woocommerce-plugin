5aed959f52a5bb5804457e0e04b0f8f3a648ddf0
Dallen Dalton <dallendalton@gmail.com>
2019-02-20 03:34:13 +0000

    Add unit test for api v3 order with exempt shipping.

--- tests/specs/test-api.php
+++ tests/specs/test-api.php
@@ -46,57 +46,13 @@ class TJ_WC_Tests_API extends WP_HTTP_TestCase {
     public function test_correct_taxes_with_shipping_api_v3() {
 
         wp_set_current_user( $this->user );
-        $product = TaxJar_Product_Helper::create_product( 'simple' )->get_id();
 
         TaxJar_Shipping_Helper::create_simple_flat_rate( 5 );
 
-        $request = new WP_REST_Request( 'POST', '/wc/v3/orders' );
-        $request->set_body_params(
-            array(
-                'payment_method'       => 'bacs',
-                'payment_method_title' => 'Direct Bank Transfer',
-                'set_paid'             => true,
-                'billing'              => array(
-                    'first_name' => 'John',
-                    'last_name'  => 'Doe',
-                    'address_1'  => '969 Market',
-                    'address_2'  => '',
-                    'city'       => 'Greenwood Village',
-                    'state'      => 'CO',
-                    'postcode'   => '80111',
-                    'country'    => 'US',
-                    'email'      => 'john.doe@example.com',
-                    'phone'      => '(555) 555-5555',
-                ),
-                'shipping'             => array(
-                    'first_name' => 'John',
-                    'last_name'  => 'Doe',
-                    'address_1'  => '969 Market',
-                    'address_2'  => '',
-                    'city'       => 'Greenwood Village',
-                    'state'      => 'CO',
-                    'postcode'   => '80111',
-                    'country'    => 'US',
-                ),
-                'line_items'           => array(
-                    array(
-                        'product_id' => $product,
-                        'quantity'   => 1
-                    )
-                ),
-                'shipping_lines'       => array(
-                    array(
-                        'method_id'    => 'flat_rate',
-                        'method_title' => 'Flat rate',
-                        'total'        => '5',
-                    ),
-                ),
-            )
-        );
+        $request = TaxJar_API_Helper::build_api_v3_request();
 
         $response = $this->server->dispatch( $request );
         $data     = $response->get_data();
-        $order    = wc_get_order( $data['id'] );
 
         $this->assertEquals( 201, $response->get_status() );
         $this->assertEquals( $data['total_tax'], 1.09, '', 0.01 );
@@ -105,6 +61,45 @@ class TJ_WC_Tests_API extends WP_HTTP_TestCase {
 
         foreach ( $data['line_items'] as $key => $item ) {
             $this->assertEquals( $item['total_tax'], 0.73, '', 0.01 );
+        }
+
+        TaxJar_Shipping_Helper::delete_simple_flat_rate();
+    }
+
+    /**
+     * Tests creating an order with exempt shipping on the V3 API
+     */
+    function test_correct_taxes_with_exempt_shipping_api_v3() {
+
+        wp_set_current_user( $this->user );
+
+        TaxJar_Shipping_Helper::create_simple_flat_rate( 5 );
+
+        $request = TaxJar_API_Helper::build_api_v3_request(
+            array(
+                'billing'              => array(
+                    'city'       => 'Santa Monica',
+                    'state'      => 'CA',
+                    'postcode'   => '90404',
+                ),
+                'shipping'             => array(
+                    'city'       => 'Santa Monica',
+                    'state'      => 'CA',
+                    'postcode'   => '90404',
+                ),
+            )
+        );
+
+        $response = $this->server->dispatch( $request );
+        $data     = $response->get_data();
+
+        $this->assertEquals( 201, $response->get_status() );
+        $this->assertEquals( $data['total_tax'], 1.03, '', 0.01 );
+        $this->assertEquals( $data['cart_tax'], 1.03, '', 0.01 );
+        $this->assertEquals( $data['shipping_tax'], 0, '', 0.01 );
+
+        foreach ( $data['line_items'] as $key => $item ) {
+            $this->assertEquals( $item['total_tax'], 1.03, '', 0.01 );
         }
 
         TaxJar_Shipping_Helper::delete_simple_flat_rate();
